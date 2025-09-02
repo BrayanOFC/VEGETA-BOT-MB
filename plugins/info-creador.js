1750c6bb2515986a2ded51de86702628b80e2182
BrayanOFC <lourdesagueda93@gmail.com>
2025-09-02 02:39:59 -0700

    Crear info-creador.js

---
+++ plugins/info-creador.js
@@ -0,0 +1,27 @@
+//créditos github.com/BrayanOFC no quitar creditos
+async function handler(m, { conn }) {
+  m.react('👑');
+
+  const name = 'BrayanOFC 👻';
+  const empresa = 'BrayanOFC - Servicios Tecnológicos';
+  const link = 'wa.me/526641784469';
+  const imagen = 'https://raw.githubusercontent.com/BrayanOFC/Adiciones/main/Contenido%2F2025090166.jpg';
+
+  const caption = `
+╭━〔 👑 *OWNER INFO* 👑 〕━⬣
+┃ 👤 *Nombre:* ${name}
+┃ 🏢 *Empresa:* ${empresa}
+┃ 🔗 *Contacto:* 
+┃    ${link}
+╰━━━━━━━━━━━━━━━━━━⬣
+  `.trim();
+
+  await conn.sendMessage(m.chat, {
+    image: { url: imagen },
+    caption,
+  }, { quoted: m });
+}
+
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = ['owner', 'creator', 'creador', 'dueño'];
