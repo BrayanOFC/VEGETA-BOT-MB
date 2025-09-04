5186b57f2487223ad85b31878179e6e86b5b60b1
BrayanOFC <lourdesagueda93@gmail.com>
2025-09-04 12:15:21 -0700

    Actualizar main-menuowner.js

--- plugins/main-menuowner.js
+++ plugins/main-menuowner.js
@@ -16,6 +16,32 @@ ${ownerHelp}
 ┗━━━━━━━━━━━━━
 `;
 
+await m.react('👑')
+
+let imgBuffer = await (await fetch('https://files.catbox.moe/ppm9t3.jpg')).buffer()
+    let media = await prepareWAMessageMedia({ image: imgBuffer }, { upload: conn.waUploadToServer })
+
+    let msg = generateWAMessageFromContent(m.chat, {
+      viewOnceMessage: {
+        message: {
+          imageMessage: {
+            ...media.imageMessage,
+            caption: menuText,
+            contextInfo: {
+              isForwarded: true,
+              forwardedNewsletterMessageInfo: {
+                newsletterJid: '120363394965381607@newsletter',
+                newsletterName: '𝚅𝙴𝙶𝙴𝚃𝙰-𝙱𝙾𝚃-𝙼𝙱 • Update',
+                serverMessageId: 101
+              }
+            }
+          }
+        }
+      }
+    }, { userJid: m.sender, quoted: m })
+
+    await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+
   m.reply(menu);
 };
 
