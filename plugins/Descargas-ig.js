a09921354891b14ec2e84e30cb36c69959652716
BrayanOFC <lourdesagueda93@gmail.com>
2025-09-04 12:54:09 -0700

    Actualizar Descargas-ig.js

--- plugins/Descargas-ig.js
+++ plugins/Descargas-ig.js
@@ -62,7 +62,7 @@ handler.limit = 0;
 delete userRequests[m.sender];
 }};
 handler.help = ['instagram *<link ig>*'];
-handler.tags = ['downloader'];
+handler.tags = ['descargas'];
 handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i;
 handler.limit = 1;
 handler.register = true;
