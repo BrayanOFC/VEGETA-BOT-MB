6600ea2cd6b5bc779fd6699dfdde09494de63a5d
BrayanOFC <lourdesagueda93@gmail.com>
2025-09-04 00:07:28 -0700

    Actualizar main-menugrupo.js

--- plugins/main-menugrupo.js
+++ plugins/main-menugrupo.js
@@ -1,15 +1,17 @@
 let handler = async (m, { conn, usedPrefix }) => {
-  let groupsCommands = Object.values(global.plugins)
+  
+let gruposHelp = Object.values(global.plugins)
     .filter(p => p?.tags?.includes('grupo') && !p.disabled)
     .map(p => {
-      let cmd = p.command instanceof RegExp ? p.command.source : Array.isArray(p.command) ? p.command.join(', ') : p.command;
-      return `• ${usedPrefix}${cmd}`;
+      // Toma solo el primer help si hay varios
+      let helpText = Array.isArray(p.help) ? p.help[0] : p.help;
+      return `• ${helpText}`;
     })
     .join('\n');
 
   let menu = `
 ┏━〔 Menú Grupos 〕━
-${groupsCommands}
+${gruposHelp}
 ┗━━━━━━━━━━━━━
 `;
 
