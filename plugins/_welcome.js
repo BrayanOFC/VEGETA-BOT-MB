f5064079debbf0c3438093a309f02908808e322c
BrayanOFC <lourdesagueda93@gmail.com>
2025-09-07 19:38:27 -0700

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -10,16 +10,16 @@ handler.before = async function (m, { conn }) {
    if (m.messageStubType == 27) {
       try {
          let groupMetadata = await conn.groupMetadata(m.chat)
-         let desc = groupMetadata.desc || "⚠️ Este grupo no tiene reglas en la descripción."
+         let desc = groupMetadata.desc || "🚀 Este grupo no tiene reglas Se Feliz Saiyajin👾."
          
-         let text = `👊🏻🔥 ¡Escucha insecto @${who.split('@')[0]}!
+         let text = `👊🏻🔥 ¡Escucha insecto/a @${who.split('@')[0]}!
 
 Has entrado al campo de batalla del grupo. Aquí no hay lugar para los débiles.  
 
 📜 **Reglas del Grupo**:
 ${desc}
 
-⚠️ El que rompa las reglas… conocerá mi furia Saiyajin. ⚠️`
+🚀 El que rompa las reglas… conocerá mi furia Saiyajin. 🚀`
 
          conn.sendMessage(m.chat, { text, mentions: [who] }, { quoted: m })
       } catch (e) {
@@ -29,7 +29,7 @@ ${desc}
 
    // ─── DESPEDIDA ───
    if (m.messageStubType == 28) {
-      let text = `💨 El guerrero @${who.split('@')[0]} ha abandonado el campo de batalla.  
+      let text = `💥 El guerrero @${who.split('@')[0]} ha abandonado el campo de batalla.  
 
 No todos soportan el poder de este grupo… ¡patético! 👊🏻🔥`
       conn.sendMessage(m.chat, { text, mentions: [who] }, { quoted: m })
