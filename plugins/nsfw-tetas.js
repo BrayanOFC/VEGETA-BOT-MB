47ea2309a149628be0be6b05672acc5a207342f0
BrayanOFC <lourdesagueda93@gmail.com>
2025-09-07 17:55:13 -0700

    Actualizar nsfw-tetas.js

--- plugins/nsfw-tetas.js
+++ plugins/nsfw-tetas.js
@@ -2,7 +2,7 @@ let handler = async(m, { conn }) => {
 
 let img = 'https://delirius-apiofc.vercel.app/nsfw/boobs';
 
-let text = '*🍭 TETAS*';
+let text = '*🫨 TETAS*';
 
 conn.sendMessage(m.chat, { image: { url: img }, caption: text }, { quoted: m });
 m.react('✅');
