f31b18f7708cbedf156291fbc27fb39e701664d6
BrayanOFC <lourdesagueda93@gmail.com>
2025-09-08 22:45:44 -0700

    Actualizar grupos-img.js

--- plugins/grupos-img.js
+++ plugins/grupos-img.js
@@ -5,34 +5,50 @@ import { tmpdir } from 'os'
 import path from 'path'
 
 let handler = async (m, { conn }) => {
-  if (!m.quoted) return m.reply('⚠️ Responde a un sticker para convertirlo en imagen.')
+  if (!m.quoted) return m.reply('⚠️ Responde a un sticker o video.')
   let mime = m.quoted.mimetype || ''
-  if (!/webp/.test(mime)) return m.reply('⚠️ Eso no parece ser un sticker.')
+  
+  if (!/webp|video/.test(mime)) return m.reply('⚠️ Solo funciona con stickers o videos.')
 
   try {
     let media = await m.quoted.download()
-    let filename = path.join(tmpdir(), `sticker_${Date.now()}.webp`)
-    fs.writeFileSync(filename, media)
+    let filename = path.join(tmpdir(), `file_${Date.now()}`)
+    fs.writeFileSync(`${filename}`, media)
 
-    let out = filename.replace('.webp', '.png')
+    let out = ''
 
-    exec(`ffmpeg -y -i "${filename}" "${out}"`, async (err) => {
-      fs.unlinkSync(filename)
-      if (err) {
-        console.error(err)
-        return m.reply('❌ Ocurrió un error al convertir el sticker.')
-      }
+    if (/webp/.test(mime)) {
+      out = `${filename}.png`
+      exec(`ffmpeg -y -i "${filename}" "${out}"`, async (err) => {
+        fs.unlinkSync(filename)
+        if (err) {
+          console.error(err)
+          return m.reply('❌ Error al convertir el sticker.')
+        }
+        let img = fs.readFileSync(out)
+        await conn.sendFile(m.chat, img, 'sticker.png', '✅ Aquí está tu foto.', m)
+        fs.unlinkSync(out)
+      })
+    } else if (/video/.test(mime)) {
+      out = `${filename}.mp4`
+      exec(`ffmpeg -y -i "${filename}" -c copy "${out}"`, async (err) => {
+        fs.unlinkSync(filename)
+        if (err) {
+          console.error(err)
+          return m.reply('❌ Error al convertir el video.')
+        }
+        let vid = fs.readFileSync(out)
+        await conn.sendFile(m.chat, vid, 'video.mp4', '✅ Aquí está tu video.', m)
+        fs.unlinkSync(out)
+      })
+    }
 
-      let img = fs.readFileSync(out)
-      await conn.sendFile(m.chat, img, 'sticker.png', '✅ Aquí está tu foto.', m)
-      fs.unlinkSync(out)
-    })
   } catch (e) {
     console.error(e)
     m.reply('❌ Error al procesar el archivo.')
   }
 }
-handler.help = ['img','jpg']
+handler.help = ['img','mp4']
 handler.tags = ['grupo']
 handler.command = ['jpg','img']
 export default handler
