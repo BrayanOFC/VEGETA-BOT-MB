0894b65b98e6dfad0958fc53235660c8f7c20d72
BrayanOFC <lourdesagueda93@gmail.com>
2025-09-13 10:52:32 -0700

    Actualizar grupos-kick.js

--- plugins/grupos-kick.js
+++ plugins/grupos-kick.js
@@ -1,24 +1,32 @@
-let handler = async (m, { conn, usedPrefix, command, isROwner }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
+  try {
+    if (!m.mentionedJid[0] && !m.quoted) {
+      return m.reply(`🐉 Ingresa el tag de un usuario. Ejemplo:\n\n*${usedPrefix + command}* @tag`)
+    }
 
-if (!m.mentionedJid[0] && !m.quoted) 
-    return m.reply(`🐉 Ingresa el tag de un usuario. Ejemplo:\n\n*${usedPrefix + command}* @tag`) 
+    let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+    if (user === conn.user.jid) {
+      return m.reply(`☁️ No puedo hacer un auto kick`)
+    }
 
-if (conn.user.jid.includes(user)) 
-    return m.reply(`☁️ No puedo hacer un auto kick`)
+    let creadorJids = global.creador.map(([id]) => id + '@s.whatsapp.net')
+    if (creadorJids.includes(user)) {
+      return m.reply(`⚡ No puedes expulsar al creador del bot`)
+    }
 
-let creadorJids = global.creador.map(([id]) => id + '@s.whatsapp.net')
-if (creadorJids.includes(user)) 
-    return m.reply(`⚡ No puedes expulsar al creador del bot`)
-await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`🔮 Usuario eliminado con éxito`) 
+    await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+    m.reply(`🔮 Usuario eliminado con éxito`)
 
+  } catch (e) {
+    console.error(e)
+    m.reply('❌ Ocurrió un error al intentar expulsar al usuario.')
+  }
 }
 
 handler.help = ['kick @user']
 handler.tags = ['grupo']
-handler.command = ['kick', 'expulsar'] 
+handler.command = ['kick', 'expulsar']
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
